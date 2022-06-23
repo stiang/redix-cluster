6b7c00fd6d4dfd9edebc9e7a43f4657f1231dfb5
Kim Grytøyr <kim@grytoyr.io>
2022-06-23 14:37:38 +0200

    Update version to 1.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RedixCluster.Mixfile do
   def project do
     [
       app: :redix_cluster_remastered,
-      version: "1.2.1",
+      version: "1.2.2",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
